da33a6c207fb47b92cb81662b1121b65b02ffa4b
J-Shaka <bed-com-13-18@unima.ac.mw>
2023-06-28 23:44:37 +0200

    modifying explore career page

--- app/src/main/java/com/example/tsogolo/ui/explore/Explore.kt
+++ app/src/main/java/com/example/tsogolo/ui/explore/Explore.kt
@@ -1,6 +1,9 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package com.example.tsogolo.ui.explore
 
 import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -19,6 +22,7 @@ import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tsogolo.model.Category
+import com.example.tsogolo.ui.career.CareerDescriptionActivity
 import com.example.tsogolo.ui.components.*
 import kotlinx.coroutines.delay
 
@@ -32,7 +36,8 @@ fun exploreCareer(
     val context = LocalContext.current
     val categoryList by exploreViewModel.categor
     var showLoader by remember { mutableStateOf(true) }
-    
+    val careers = exploreViewModel.career.value
+
     LaunchedEffect(Unit) {
         exploreViewModel.initialize(context)
         delay(1000) // Set the waiting time (e.g., 1 seconds)
@@ -42,50 +47,157 @@ fun exploreCareer(
     Column(modifier = Modifier.background(color = MaterialTheme.colors.background).fillMaxSize()) {
 
 
-            TopAppBar(
-                title = {
+        TopAppBar(
+            title = {
 
-                        Text("Categories")
+                Text("Categories")
 
-                },
-                backgroundColor = MaterialTheme.colors.background,
-                contentColor = Color.Black,
-                navigationIcon = {
+            },
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = Color.Black,
+            navigationIcon = {
 
-                        IconButton(onClick = { backArrowClicked() }) {
-                            Icon(
-                                imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Back Arrow",
-                                tint = Color.Black
-                            )
-                        }
-
+                IconButton(onClick = { backArrowClicked() }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back Arrow",
+                        tint = Color.Black
+                    )
                 }
 
-            )
+            }
+
+        )
+        Divider(modifier = Modifier.padding(vertical = 4.dp))
+
+        Column(modifier = Modifier.padding(horizontal = 2.dp)) {
+//            Text(text = buildAnnotatedString {
+//                appendInlineContent("filter")
+//                append("Filter Careers")
+//            }, inlineContent = mapOf("filter" to InlineTextContent(
+//                Placeholder(20.sp, 20.sp, PlaceholderVerticalAlign.TextCenter)
+//            ) {
+//                Icon(
+//                    painter = painterResource(id = R.drawable.ic_baseline_filter_list_24),
+//                    contentDescription = "",
+//                    modifier = Modifier.fillMaxWidth(),
+//                )
+//            }), modifier = Modifier.padding(4.dp),
+//                style = Typography.caption.copy(
+//                    fontFamily = FontFamily.SansSerif
+//                )
+//            )
+
+//            OutlinedTextField(
+//                value = exploreViewModel.keyword.value,
+//                onValueChange = { exploreViewModel.onKeywordChange(it) },
+//                placeholder = { Text(text = "Type Keyword") },
+//                modifier = Modifier.fillMaxWidth(),
+//                textStyle = Typography.body1.copy(color = MaterialTheme.colors.onSurface)
+//            )
+
+            Row {
+
+                FilterChip(
+                    selected = exploreViewModel.filterByCat.value,
+                    onClick = { exploreViewModel.filterByCatClicked() },
+                    colors = ChipDefaults.filterChipColors(
+                        selectedBackgroundColor = Color(0xFF0eF7729)
+                    )
+                ) {
+                    Text(text = "category")
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                FilterChip(
+                    selected = exploreViewModel.filterByGrades.value,
+                    onClick = { exploreViewModel.filterByGradesClicked() },
+                    colors = ChipDefaults.filterChipColors(
+                        selectedBackgroundColor = Color(0xFF0eF7729)
+                    )
+                ) {
+                    Text(text = "career"
+                    )
+                }
+            }
+        }
 
 
-                if (showLoader) {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator() // Show the loader
-                    }
-                } else {
-                    if (categoryList.isNotEmpty()) {
 
-                        CareerCategoryList(categoryList) { categoryId ->
-                            // Handle category click event here
-                            val intent = Intent(context, CategoryDetailsActivity::class.java)
-                            intent.putExtra("category_id", categoryId.toInt())
-                            context.startActivity(intent)
-                        }
+        if (showLoader) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator() // Show the loader
+            }
+        } else {
+            if (categoryList.isNotEmpty() && exploreViewModel.filterByCat.value) {
+
+//                        CareerCategoryList(categoryList) { categoryId ->
+//                            // Handle category click event here
+//                            val intent = Intent(context, CategoryDetailsActivity::class.java)
+//                            intent.putExtra("category_id", categoryId.toInt())
+//                            context.startActivity(intent)
+//                        }
+//                        CareerCategoryList(categoryList) { categoryId ->
+//                            // Handle category click event here
+//                            exploreViewModel.selectedCategoryId = categoryId.toInt()
+//                        }
+
+                CareerCategoryList(categoryList, exploreViewModel) { categoryId ->
+//                            exploreViewModel.selectedCategoryId = categoryId.toInt()
+                    // Handle category click event here
+                    exploreViewModel.reinitialize(context)
+
+
+                    val currentSelection = exploreViewModel.selectedCategoryId
+                    if (currentSelection == categoryId.toInt()) {
+                        // Clicked again on the same category, toggle the selection off
+                        exploreViewModel.selectedCategoryId = null
                     } else {
-                        Text(
-                            text = "No categories available",
-                            modifier = Modifier.fillMaxSize().padding(16.dp),
-                            textAlign = TextAlign.Center
-                        )
+                        // Clicked on a different category, toggle the selection on
+                        exploreViewModel.selectedCategoryId = categoryId.toInt()
                     }
                 }
+
+
+
+            }
+            else if (careers.isNotEmpty() && exploreViewModel.filterByGrades.value) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(careers) { career ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(2.dp)
+                                .clickable {
+                                    career.id.let {
+                                        Log.d("CategoryViewModel", "tione: ${career.id}")
+                                        val intent = Intent(context, CareerDescriptionActivity::class.java).apply {
+                                            putExtra("careerId", career.id)
+                                        }
+                                        context.startActivity(intent)
+                                    }
+                                },
+                            elevation = 4.dp
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                            ) {
+                                // Display career details here inside the card
+                                career.title?.let { Text(text = it) }
+                            }
+                        }
+                    }
+                }
+            } else {
+                Text(
+                    text = "No categories available",
+                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
 
 
     }
@@ -93,32 +205,64 @@ fun exploreCareer(
 
 
 @Composable
-fun CareerCategoryList(careerCategories: List<Category>, onCategoryClicked: (String) -> Unit) {
-    LazyColumn {
-        items(careerCategories) { category ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(2.dp)
-                    .clickable { onCategoryClicked(category.id.toString()) },
-                elevation = 4.dp
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
+fun CareerCategoryList(
+    careerCategories: List<Category>,
+    exploreViewModel: ExploreViewModel,
+    onCategoryClicked: (String) -> Unit
+) {
+    val careers = exploreViewModel.careers.value
+    val context = LocalContext.current // Get the current context
+
+    Box(
+        modifier = Modifier
+            .background(color = Color(0xFFEEEEEE)) // Set the background color for the Box
+            .fillMaxSize()
+    ) {
+        LazyColumn {
+            items(careerCategories) { category ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(2.dp)
+                        .clickable(enabled = exploreViewModel.isInitialized) { onCategoryClicked(category.id.toString()) },
+                    elevation = 4.dp
                 ) {
-//                    Text(
-//                        text = "Category ID: ${category.id}",
-//                        style = MaterialTheme.typography.subtitle1
-//                    )
-                    Text(
-                        text = " ${category.categoryName}",
-                        style = MaterialTheme.typography.body1
-                    )
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            text = " ${category.categoryName}",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+
+                val currentSelection = exploreViewModel.selectedCategoryId
+                if (currentSelection != null && currentSelection == category.id) {
+                    careers.forEach { career ->
+                        Box(
+                            modifier = Modifier
+                                .clickable {
+                                    career.id.let {
+                                        val intent = Intent(context, CareerDescriptionActivity::class.java).apply {
+                                            putExtra("careerId", it)
+                                        }
+                                        context.startActivity(intent)
+                                    }
+                                }
+                                .padding(16.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Column {
+                                Text(text = career.title)
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+
 }
+
 
 
 
--- app/src/main/java/com/example/tsogolo/ui/explore/ExploreViewModel.kt
+++ app/src/main/java/com/example/tsogolo/ui/explore/ExploreViewModel.kt
@@ -4,7 +4,9 @@ package com.example.tsogolo.ui.explore
 import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.tsogolo.database.TsogoloDatabase
 import com.example.tsogolo.model.Career
@@ -17,32 +19,55 @@ import kotlinx.coroutines.launch
 
 class ExploreViewModel : ViewModel() {
     val categor: MutableState<List<Category>> = mutableStateOf(listOf())
+    val filterByGrades: MutableState<Boolean> = mutableStateOf(false)
+    val filterByCat: MutableState<Boolean> = mutableStateOf(true)
     val keyword: MutableState<String> = mutableStateOf("")
     val selectedCareers: MutableState<List<Career>> = mutableStateOf(listOf())
     val careers: MutableState<List<CareerData>> = mutableStateOf(listOf())
+    val career: MutableState<List<Career>> = mutableStateOf(listOf())
+    //    val career: MutableState<List<CareerData>> = mutableStateOf(listOf())
+    var selectedCategoryId: Int? by mutableStateOf(null)
 
 //    val careerCategor: MutableState<List<CategoryWithCareer>> = mutableStateOf(listOf())
 
     private lateinit var db: TsogoloDatabase
-//    private lateinit var db: TsogoloDatabase
+    //    private lateinit var db: TsogoloDatabase
     private var _careers: List<Career> = listOf()
+
+    var isInitialized: Boolean by mutableStateOf(false) // Flag to track initialization status
+
 
     fun initialize(context: Context) {
         db = TsogoloDatabase.getInstance(context)
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                _careers = db.careerDao().getAll()
+                var ch = 0
+                if(selectedCategoryId != null){
+                    _careers = db.careerDao().getCareersByCategory(selectedCategoryId!!)
+                }
+
                 val categoryList = db.categoryDao().getAll()
                 val careerCategory = db.categoryDao().careerCategor()
+
+                career.value = db.careerDao().getAll()
+
+
                 categor.value = categoryList
                 mapCareers()
+
+                isInitialized = true
 //                careerCategor.value = careerCategory
-                Log.d("ExploreViewModel", "Category data retrieved: $careerCategory")
+                Log.d("ExploreViewModel", "Category data retrieved: $selectedCategoryId")
             } catch (e: Exception) {
                 Log.e("ExploreViewModel", "Failed to retrieve category data: ${e.message}")
             }
         }
+    }
+
+    fun reinitialize(context: Context) {
+        isInitialized = false
+        initialize(context)
     }
 
     private fun filtered(careers: List<CareerData>): List<CareerData> {
@@ -55,9 +80,34 @@ class ExploreViewModel : ViewModel() {
         }
     }
 
+//    val filteredCareers: List<CareerData>
+//        get() {
+//            val selectedId = selectedCategoryId
+//            return if (selectedId != null) {
+//                careers.value.filter { it.categoryId == selectedId }
+//            } else {
+//                careers.value
+//            }
+//        }
+
+//    val filteredCareers: List<CareerData>
+//        get() {
+//            val selectedId = selectedCategoryId
+//            return if (selectedId != null) {
+//                careers.value.filter { career ->
+//                    // Apply your custom logic to filter based on selectedId
+//                    // For example, you can check if the career's title contains the selectedId
+//                    career.title.contains(selectedId.toString(), ignoreCase = true)
+//                }
+//            } else {
+//                careers.value
+//            }
+//        }
+
     private fun mapCareers() {
         CoroutineScope(Dispatchers.Default).launch {
             this@ExploreViewModel.careers.value = filtered(_careers.map { career ->
+
                 CareerData(
                     id = career.id!!,
                     title = career.title ?: "Unknown",
@@ -75,6 +125,18 @@ class ExploreViewModel : ViewModel() {
         Log.d("ExploreViewModel", "Changed: $keyword")
         // Perform filtering and update the careers list
         mapCareers()
+    }
+
+    fun filterByGradesClicked() {
+        filterByGrades.value = true; // Update the value of filterByGrades
+        filterByCat.value = false;
+        mapCareers();
+    }
+
+    fun filterByCatClicked() {
+        filterByCat.value = true; // Update the value of filterByGrades
+        filterByGrades.value = false;
+        mapCareers();
     }
 }
 
